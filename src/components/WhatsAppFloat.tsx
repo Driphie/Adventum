cd9a9e92d702b8992945907905f351fd59751a30
Driphie Driphiee <gonza.10.cs@hotmail.com>
2025-08-11 23:25:55 -0300

    agregando wp

--- src/components/WhatsAppFloat.tsx
+++ src/components/WhatsAppFloat.tsx
@@ -2,8 +2,8 @@ const whatsappLogo = "/images/wp.png";
 
 const WhatsAppFloat = () => {
   const handleWhatsAppClick = () => {
-    const phoneNumber = "541123482822";
-    const message = "mensaje de prueba";
+    const phoneNumber = "541124681383";
+    const message = "Buenas vengo desde la pagina web de ADVENTUM y queria informarme acerca del proyecto.";
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
   };
@@ -12,10 +12,10 @@ const WhatsAppFloat = () => {
     <div className="fixed bottom-6 right-6 z-[9999]">
       {/* Pulse animation background with gradient */}
       <div className=""></div>
-      
+
       {/* Outer glow ring */}
       <div className=""></div>
-      
+
       {/* WhatsApp button with enhanced design */}
       <button
         onClick={handleWhatsAppClick}
@@ -25,7 +25,7 @@ const WhatsAppFloat = () => {
       >
         {/* Inner highlight */}
         <div className="absolute inset-1 bg-gradient-to-t from-transparent to-white/30 rounded-full pointer-events-none"></div>
-        
+
         <img 
           src={whatsappLogo} 
           alt="WhatsApp Logo"
